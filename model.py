31b31945e9f971d3c9a3c69e91fb27c0b416355b
COOLSTORM <yoyo.sunshine@hotmail.com>
2026-08-25 12:54:38 +0100

    Update model.py

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # ============================================================
 
 st.set_page_config(
-    page_title="Noodle Shop Business Model",
+    page_title="Shop Business Model",
     page_icon="🍜",
     layout="wide"
 )
